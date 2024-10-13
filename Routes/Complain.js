5812da84bb002fe345a837b609271eb42b897cd4
Andi-gech <andi.fab23@gmail.com>
2024-10-13 04:00:42 -0400

    complain

--- Routes/Complain.js
+++ Routes/Complain.js
@@ -6,20 +6,36 @@ const Authetication = require("../MiddleWare/AuthMiddleware");
 const { roleAuth } = require("../MiddleWare/RoleAuth");
 
 Router.post("/", Authetication, roleAuth("student"), async (req, res) => {
-try {
+  try {
     const { error } = validateComplain(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     const complain = new Complain({
-        complain: req.body.complain,
-        type: req.body.type,
-        user: req.user._id
+      complain: req.body.complain,
+      type: req.body.type,
+      user: req.user._id,
     });
     await complain.save();
     return res.send(complain);
-
-} catch (error) {
+  } catch (error) {
     return res.status(500).send(error.message || "Something went wrong");
-}
-})
+  }
+});
+Router.get("/", async (req, res) => {
+  try {
+    const complain = await Complain.find();
+    return res.send(complain);
+  } catch (error) {
+    return res.status(500).send;
+  }
+});
+Router.delete("/:id", async (req, res) => {
+  try {
+    const complain = await Complain.findByIdAndDelete(req.params.id);
+    if (!complain) return res.status(400).send("Complain not found");
+    return res.send(complain);
+  } catch (error) {
+    return res.status(500).send(error.message || "Something went wrong");
+  }
+});
 
-module.exports = Router
+module.exports = Router;
